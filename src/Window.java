0b3cc921c8c29680df17c504ef055acdc69628c9
nicrausaz <n.crausaz99@gmail.com>
2022-02-23 10:37:44 +0100

    Base window

---
+++ src/Window.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Window
+{
+    private Board board = new Board();
+
+    public Window () {
+        JFrame frame = new JFrame("Bouncers");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setPreferredSize(new Dimension(400, 300));
+
+        frame.pack();
+
+        frame.setVisible(true);
+    }
+}
